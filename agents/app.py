ddfbb7941677caf35f00994fb487a8368e2c851f
Fernando <fernando.sacerdote@hotmail.com>
2024-11-19 13:53:03 -0300

    integrando o front com a api

--- agents/app.py
+++ agents/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import base_agent
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route("/api/message", methods=['POST'])
 def data():
